7556bee54b58f1a980fe2ced6d4492daa6c1a9dc
hanseul-Choi <58256559+hanseul-Choi@users.noreply.github.com>
2021-07-03 11:50:00 +0900

    짝수와 홀수

---
+++ level1_kotlin/짝수와 홀수/EvenOdd.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(num: Int): String {
+        var answer = ""
+        
+        if(num % 2 == 0) answer = "Even"
+        else answer = "Odd"
+        
+        return answer
+    }
+}
